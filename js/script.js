029866a37f4b73c23178c3581b52c84d1d4892f3
wagnysson <wagnyssonmoura@gmail.com>
2021-04-02 20:15:27 -0300

    responsivo

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const sandwich = document.querySelector('.sandwich')
+const menupc = document.querySelector('.itemmenu')
+const menul = document.querySelector('.menul')
+const menur = document.querySelector('.menur')
+const menu = document.querySelector('.menu')
+const fundomenu = document.querySelector('.fundomenu')
+
+
+sandwich.addEventListener('click', function() {
+    if (menupc.style.display !== 'flex') {
+        menupc.style.display = 'flex'
+        menul.style.display = 'flex'
+        menur.style.display = 'flex'
+        menu.style.height = '150px'
+        fundomenu.style.display = 'block'
+    } else {
+        menupc.style.display = ''
+        menul.style.display = ''
+        menur.style.display = ''
+        menu.style.height = ''
+        fundomenu.style.display = ''
+    }
+})
